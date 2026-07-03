6c5d72661e1c8a398205e08142ed2dd0d462046f
kagurazakei <maotsugiri@gmail.com>
2026-07-03 11:11:14 +0630

    update: flake.nix and shell.nix

--- flake.nix
+++ flake.nix
@@ -8,14 +8,10 @@
       inputs = (import ./.tack) { overrides = args.tackOverrides or { }; };
       pkgs = import inputs.nixpkgs { inherit system; };
       config = import ./default.nix;
-      devShell = import ./shell.nix {
-        inherit pkgs inputs self;
-        config = config;
-      };
 
     in
     {
       nixosConfigurations = config.nC or { };
-      devShells.${system}.default = devShell;
+      devShells.${system}.default = import ./shell.nix { inherit pkgs inputs; };
     };
 }
